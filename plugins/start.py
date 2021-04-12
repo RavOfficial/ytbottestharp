dae34893812d19634ec2d53033f769fc0b586345
Bawwa Official <78470759+OfficialBawwa@users.noreply.github.com>
2021-04-12 17:07:09 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,12 +1,79 @@
-from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+import os
+import asyncio
+from urllib.parse import urlparse
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from youtube_dl import YoutubeDL
+from opencc import OpenCC
+from config import Config
 
 
-@Client.on_message(Filters.command(["start"]), group=-2)
+@BellaYT.on_message(filters.command("start"))
 async def start(client, message):
-    # return
-    joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("HARP Tech 🇱🇰", url="https://t.me/Harp_Tech")]
-    ])
-    welcomed = f"<b>Hey {message.from_user.first_name} \nIf you need help 🙄 Just click /help\n\n</b>"
-    await message.reply_text(welcomed, reply_markup=joinButton)
-    raise StopPropagation
+   if message.chat.type == 'private':
+       await Jebot.send_message(
+               chat_id=message.chat.id,
+               text="""<b>Youtube Downloader Bot Help!
+Just send a Youtube url to download it in video or audio format!
+If you have any question?
+Report us [here!](https://t.me/HARP_Chat)
+Another bot by [HARP Tech 🇱🇰](https://t.me/Tech)
+Join now with us @HARP_Tech 🇱🇰 </b>""",   
+                            reply_markup=InlineKeyboardMarkup(
+                                [[
+                                        InlineKeyboardButton(
+                                            "Help ❔", callback_data="help"),
+                                        InlineKeyboardButton(
+                                            "Channel", url="https://t.me/HARP_Tech")
+                                    ],[
+                                      InlineKeyboardButton(
+                                            "Group", url="https://t.me/HARP_Chat")
+                                    ]]
+                            ),        
+            disable_web_page_preview=True,        
+            parse_mode="html")
+
+@BellaYT.on_message(filters.command("help"))
+async def help(client, message):
+    if message.chat.type == 'private':   
+        await BellaYT.send_message(
+               chat_id=message.chat.id,
+               text="""<b>Youtube Downloader Bot Help!
+Just send a Youtube url to download it in video or audio format!
+If you have any question?
+Report us [here!](https://t.me/HARP_Chat)
+Another bot by [HARP Tech 🇱🇰](https://t.me/HARP_Tech)
+Join now with us @HARP_Chat 🇱🇰 </b>""",
+        reply_markup=InlineKeyboardMarkup(
+                                [[
+                                        InlineKeyboardButton(
+                                            "Back", callback_data="start"),
+                                        InlineKeyboardButton(
+                                            "About", callback_data="about"),
+                                  ],[
+                                        InlineKeyboardButton(
+                                            "Join now! 🇱🇰 ", url="https://t.me/HARP_Tech")
+                                    ]]
+                            ),        
+            disable_web_page_preview=True,        
+            parse_mode="html")
+
+@BellaYT.on_message(filters.command("about"))
+async def about(client, message):
+    if message.chat.type == 'private':   
+        await Jebot.send_message(
+               chat_id=message.chat.id,
+               text="""<b>About Youtube Downloader Bot!</b>
+<b>Creator:</b> <a href="https://t.me/HARP_Tech">HARP Tech 🇱🇰</a>
+<b>Get a help by contacting us:</b> <a href="https://t.me/HARP_Chat">HARP Tech's official support group</a>
+<b>Join now with us @HARP_Chat 🇱🇰</b>""",
+     reply_markup=InlineKeyboardMarkup(
+                                [[
+                                        InlineKeyboardButton(
+                                            "Back", callback_data="help"),
+                                        InlineKeyboardButton(
+                                            "Join now! 🇱🇰 ", url="https://t.me/HARP_Tech")
+                                    ]]
+                            ),        
+            disable_web_page_preview=True,        
+            parse_mode="html")
